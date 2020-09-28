7f72e505657552f504f6edff49f601d73d2dc173
matvoy <plikhivskyi.matviy@gmail.com>
2020-09-28 14:29:57 +0300

    add models

---
+++ internal/repo/models.go
@@ -0,0 +1,31 @@
+package repo
+
+import (
+	"time"
+)
+
+type Conversation struct {
+	ID        int64      `json:"id"`
+	Title     *string    `json:"title,omitempty"`
+	CreatedAt *time.Time `json:"created_at,omitempty"`
+	ClosedAt  *time.Time `json:"closed_at,omitempty"`
+	UpdatedAt *time.Time `json:"updated_at,omitempty"`
+	DomainID  int64      `json:"domain_id"`
+	Members   []*Member  `json:"members"`
+}
+
+type Member struct {
+	ChannelID int64  `json:"channel_id"`
+	UserID    int64  `json:"user_id"`
+	Username  string `json:"username"`
+	Internal  bool   `json:"internal"`
+	Firstname string `json:"firstname,omitempty"`
+	Lastname  string `json:"lastname,omitempty"`
+}
+
+// type Message struct {
+// 	ChannelID int64 `json:"channel_id"`
+// 	// UserID    int64  `json:"user_id"`
+// 	Value string `json:"value"`
+// 	Type  string `json:"type"`
+// }
--- internal/repo/pg/conversation.go
+++ internal/repo/pg/conversation.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"time"
 
+	"github.com/matvoy/chat_server/internal/repo"
 	"github.com/matvoy/chat_server/models"
 
 	"github.com/volatiletech/null/v8"
@@ -25,12 +26,12 @@ import (
 // 	return result, nil
 // }
 
-func (repo *PgRepository) GetConversationByID(ctx context.Context, id int64) (*models.Conversation, error) {
+func (r *PgRepository) GetConversationByID(ctx context.Context, id int64) (*models.Conversation, error) {
 	result, err := models.Conversations(
 		models.ConversationWhere.ID.EQ(id),
-	).One(ctx, repo.db)
+	).One(ctx, r.db)
 	if err != nil {
-		repo.log.Warn().Msg(err.Error())
+		r.log.Warn().Msg(err.Error())
 		if err == sql.ErrNoRows {
 			return nil, nil
 		}
@@ -39,20 +40,20 @@ func (repo *PgRepository) GetConversationByID(ctx context.Context, id int64) (*m
 	return result, nil
 }
 
-func (repo *PgRepository) CreateConversation(ctx context.Context, c *models.Conversation) error {
-	if err := c.Insert(ctx, repo.db, boil.Infer()); err != nil {
+func (r *PgRepository) CreateConversation(ctx context.Context, c *models.Conversation) error {
+	if err := c.Insert(ctx, r.db, boil.Infer()); err != nil {
 		return err
 	}
 	return nil
 }
 
-func (repo *PgRepository) CloseConversation(ctx context.Context, id int64) error {
+func (r *PgRepository) CloseConversation(ctx context.Context, id int64) error {
 	// result, err := models.Conversations(qm.Where("LOWER(session_id) like ?", strings.ToLower(sessionID)), qm.Where("closed_at is null")).
 	// 	One(ctx, repo.db)
 	result, err := models.Conversations(models.ConversationWhere.ID.EQ(id)).
-		One(ctx, repo.db)
+		One(ctx, r.db)
 	if err != nil {
-		repo.log.Warn().Msg(err.Error())
+		r.log.Warn().Msg(err.Error())
 		if err == sql.ErrNoRows {
 			return nil
 		}
@@ -62,10 +63,48 @@ func (repo *PgRepository) CloseConversation(ctx context.Context, id int64) error
 		Valid: true,
 		Time:  time.Now(),
 	}
-	_, err = result.Update(ctx, repo.db, boil.Infer())
+	_, err = result.Update(ctx, r.db, boil.Infer())
 	return err
 }
 
-func (repo *PgRepository) GetConversations(ctx context.Context, limit, offset int) ([]*models.Conversation, error) {
-	return models.Conversations(qm.Limit(limit), qm.Offset(offset)).All(ctx, repo.db)
+func (r *PgRepository) GetConversations(ctx context.Context, limit, offset int) ([]*repo.Conversation, error) {
+	conversations, err := models.Conversations(qm.Limit(limit), qm.Offset(offset), qm.Load(models.ConversationRels.Channels)).All(ctx, r.db)
+	if err != nil {
+		return nil, err
+	}
+	result := make([]*repo.Conversation, 0, len(conversations))
+	for _, c := range conversations {
+		members := make([]*repo.Member, 0, len(c.R.Channels))
+		for _, ch := range c.R.Channels {
+			if !ch.Internal {
+				client, err := models.Clients(models.ClientWhere.ID.EQ(ch.UserID)).One(ctx, r.db)
+				if err != nil {
+					return nil, err
+				}
+				members = append(members, &repo.Member{
+					ChannelID: ch.ID,
+					UserID:    ch.UserID,
+					Username:  client.Name.String,
+					Firstname: client.FirstName.String,
+					Lastname:  client.LastName.String,
+				})
+			} else {
+				members = append(members, &repo.Member{
+					ChannelID: ch.ID,
+					UserID:    ch.UserID,
+				})
+			}
+
+		}
+		result = append(result, &repo.Conversation{
+			ID:        c.ID,
+			Title:     &c.Title.String,
+			CreatedAt: &c.CreatedAt.Time,
+			ClosedAt:  &c.ClosedAt.Time,
+			UpdatedAt: &c.UpdatedAt.Time,
+			DomainID:  c.DomainID,
+			Members:   members,
+		})
+	}
+	return result, nil
 }
